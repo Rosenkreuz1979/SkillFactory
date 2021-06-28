75c4cd1f3b49172c9a3d98e53a07da6687f9287a
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-06-28 20:37:29 +0700

    Commit

---
+++ Landing Page/script/6-1.js
@@ -0,0 +1,12 @@
+function evenOdd() { // 6.1
+    let myArray = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), null , Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), "девятнадцать", null , Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), null, "четыре"];
+    
+    let arrayOdd = myArray.filter( item => item % 2 !== 0  && item !== null && item !== 0 && typeof(item) == "number");
+    let arrayEven = myArray.filter( item => item % 2 == 0  && item !== null  && item !== 0 && typeof(item) == "number");
+    let arrayEmpty = myArray.filter(item => !item );
+    
+    console.log('В случайно (почти) сгенерированном Массиве на ' + myArray.length + ' элементов:');
+    console.log('\n Закрались четные элементы в количестве ' + arrayEven.length);
+    console.log('\n И нечетные ' + arrayOdd.length);
+    console.log('\n Пустых - ' + arrayEmpty.length);
+}
