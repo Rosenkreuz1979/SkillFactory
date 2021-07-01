eb14bd1bba8b4a5156127cccdeaade8894cc9323
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-07-01 14:14:29 +0700

    1

---
+++ slider/script/main.js
@@ -0,0 +1,28 @@
+let images = [{
+    url: "https://img.favcars.com/mini/hatch/mini_hatch_2010_wallpapers_14_1280x960.jpg",
+    title: "Mini Cooper черный"
+  }, {
+    url: "https://img.favcars.com/mini/cabrio/mini_cabrio_2009_pictures_5_1280x960.jpg",
+    title: "Mini Cooper красный"
+  }, {
+    url: "https://www.t-r-n.ru/files/modification-images/cb/a8/5c/f9/40061_tmb940.jpg",
+    title: "Mini Cooper синий"
+  }, {
+    url: "https://i.pinimg.com/736x/c5/d9/14/c5d9142556fe74c49a2c1c2d4ea6d46a.jpg",
+    title: "Mini Cooper бордовый"
+  }, {
+    url: "https://auusaca.com/wp-content/uploads/2019/12/6.jpg",
+    title: "Mini Cooper белый"
+}];
+
+function initSlider() {
+    if (!images || !images.length) return;
+
+    let sliderImages=document.querySelector(".slider_images");
+    let sliderArrows=document.querySelector(".slider_arrows");
+
+    
+
+}
+
+document.addEventListener("DOMContentLoaded", initSlider);
