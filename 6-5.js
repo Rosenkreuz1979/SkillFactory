7bf97629ad1ac3c201ce93825e655f246dc27b33
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-07-03 00:10:23 +0700

    10

--- 6-5.js
+++
@@ -1,6 +0,0 @@
-let number1=Math.floor(Math.random() * 100);
-let number2=Math.floor(Math.random() * 100);
- 
-const powerOfNumber = (a,b) => Math.pow(a,b);
-
-console.log(`${number1} в степени ${number2} = ${powerOfNumber(number1,number2)}`);
