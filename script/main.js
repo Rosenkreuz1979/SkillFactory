407540992547229a03651630770e749252f4a32d
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-07-03 11:36:52 +0700

    10-1

--- script/main.js
+++ script/main.js
@@ -4,23 +4,13 @@ let icon1 = button.querySelector(".button-icon");
 let icon2 = button.querySelector(".hidden-icon");
 
 function switchVisibility(){
-    if (icon1.classList.contains("visible")) {
-        icon1.classList.remove("visible");
-        icon1.classList.add("hidden");
-        icon2.classList.remove("hidden");
-        icon2.classList.add("visible");        
-    } else {
-        icon2.classList.remove("visible");
-        icon2.classList.add("hidden");
-        icon1.classList.remove("hidden");
-        icon1.classList.add("visible");  
-    }
+    icon1.classList.toggle("hidden");
+    icon2.classList.toggle("hidden");
 }
 
 
 button.addEventListener("click", function(){
-    switchVisibility();
-    
+    switchVisibility();    
 })
 
 }
