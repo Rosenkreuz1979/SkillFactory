762d303e3ddd6c94a5ae4592797ce82ce8d0f712
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-07-03 17:34:00 +0700

    1

--- script/main.js
+++ script/main.js
@@ -19,5 +19,69 @@ button2.addEventListener("click", () => { //10-2
    // alert(`Размеры окна: Ширина:${window.innerWidth} Высота:${window.innerHeight}`); //окно
 })
 
+/* Chat Realisation */
+let webURL = "wss://echo.websocket.org/";
+
+
+let chatSend = document.querySelector('.chat-send');
+let chatGeo = document.querySelector('.chat-geo');
+let chatInput = document.querySelector('.chat-input');
+let chatMessages = document.querySelector('.chat-messages');
+
+let webSocket;
+
+function wrapMessage(msg,from) {
+    if (msg) {
+        let message = document.createElement('div');
+        message.className = `message-${from}`;
+        message.innerHTML = msg;  
+        // message.innerHTML = `<div class="message-${from}">${msg}</div>`;
+        if (chatMessages.querySelector(".empty-placeholder")){            
+           chatMessages.innerHTML="";            
+        }
+        chatMessages.insertBefore(message,chatMessages.firstChild);
+        
+    }
+ 
+}
+
+wrapMessage("Hello there","server");
+wrapMessage("Hi","client");
+//wrapMessage("What are you up to?","server");
+//wrapMessage("Nothing much","client");
+
+
+/*btnOpen.addEventListener('click', () => {
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function(evt) {
+    writeToScreen("CONNECTED");
+  };
+  websocket.onclose = function(evt) {
+    writeToScreen("DISCONNECTED");
+  };
+  websocket.onmessage = function(evt) {
+    writeToScreen(
+      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
+    );
+  };
+  websocket.onerror = function(evt) {
+    writeToScreen(
+      '<span style="color: red;">ERROR:</span> ' + evt.data
+    );
+  };
+});
+
+btnClose.addEventListener('click', () => {
+  websocket.close();
+  websocket = null;
+});
+
+btnSend.addEventListener('click', () => {
+  const message = 'Test message';
+  writeToScreen("SENT: " + message);
+  websocket.send(message);
+});
+
+*/
 
 }
