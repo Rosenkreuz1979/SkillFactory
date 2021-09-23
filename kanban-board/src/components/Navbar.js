90431edaae162a612ec3ccfdd07d3faa89fd70cb
Rosenkreuz1979 <alexey.krasnichenko@gmail.com>
2021-09-23 12:14:09 +0700

    Commit

--- kanban-board/src/components/Navbar.js
+++ kanban-board/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return(
       <div className="navbar">
+        Awesome Kanban Board <i className="fa-solid fa-clipboard-list-check"></i>
 
       </div>
   )
--- kanban-board/src/index.js
+++ kanban-board/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 ReactDOM.render(
   <>
+    <Navbar/>
+
     <App />
-    <App />
-    <App />
-    <App />
+    <Footer />
   </>,
   document.getElementById('root')
 );
